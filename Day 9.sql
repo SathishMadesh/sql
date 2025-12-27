baa7abb71592ff9a00148c002428d90144a06f29
SathishMadesh <s747272946@gmail.com>
2025-12-27 22:37:47 +0530

    window functions

--- Day 9.sql
+++ Day 9.sql
@@ -11,7 +11,25 @@ group by gender;
 
 -- using window function
 
-select gender, avg(salary) over(partition by gender)
+select dem.first_name, dem.last_name, avg(salary) over(partition by gender)
+from employee_demographics as dem
+join employee_salary as sal
+	on dem.employee_id = sal.employee_id
+;
+
+
+select dem.first_name, gender, dem.employee_id,
+sum(salary) over(partition by gender order by dem.employee_id) as roling_total
+from employee_demographics as dem
+join employee_salary as sal
+	on dem.employee_id = sal.employee_id
+;
+
+
+select dem.employee_id, dem.first_name, gender, salary,
+row_number() over(order by salary desc) as row_num,
+rank() over(order by salary desc) as rank_num,
+dense_rank() over(order by salary desc) as dense_rank_num
 from employee_demographics as dem
 join employee_salary as sal
 	on dem.employee_id = sal.employee_id
